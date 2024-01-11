2796f72b13a23dfad8233edcde2e3ac1b7e215f1
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-01-11 15:33:04 +0300

    api güncellendi

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Coin from './component/Coin'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Coin from "./component/Coin";
+import "./App.css";
 
 function App() {
   const [listCoin, setListCoin] = useState([]);
   const [searchWord, setSearchWord] = useState("");
 
   async function getCrypto() {
-    const response = await fetch (
+    const response = await fetch(
       "https://api.coinstats.app/public/v1/coins?skip=0&limit=0",
-      { method: 'GET' }
-      )
+      { method: "GET" }
+    );
     const json = await response.json();
-    const data = json.coins
+    const data = json.coins;
     setListCoin(data);
-    console.log(data)
   }
 
   useEffect(() => {
-    getCrypto()
-  }, [])
+    getCrypto();
+  }, []);
 
-  const filteredCoins = listCoin.filter((coin) => {
-    return coin.name.toLowerCase().includes(searchWord.toLowerCase())
-  })
+  const filteredCoins = listCoin?.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
 
   return (
     <div className="App">
       <div className="crypto-header">
-        <input type="text" placeholder="Search" onChange={e => setSearchWord(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
       </div>
       <div className="crypto-list">
-        {filteredCoins.map((coin,key) => {
+        {filteredCoins?.map((coin, key) => {
           return (
-            <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/>
-          )
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
         })}
       </div>
     </div>
